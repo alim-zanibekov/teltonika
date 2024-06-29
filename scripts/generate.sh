3d7932af7427761ed385fd1979bbac6cecf69ef3
alimzanibekov <zanibekov.alim@gmail.com>
2024-06-29 16:37:48 +0500

    Delete unused files

--- scripts/generate.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env sh
-
-DIR=$(dirname "$0")
-OUT_FILE=$(realpath "$DIR/../ioelements/ioelements_dump.go")
-
-$DIR/elements_dump.py '["Permanent I/O elements", "Eventual I/O elements", "Bluetooth Low Energy"]' > $OUT_FILE
